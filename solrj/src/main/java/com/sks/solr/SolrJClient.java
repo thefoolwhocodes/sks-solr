2078f8668224ed69a05fe4e92b4b5c5af963963c
Shashi Kant <shashikantsantosh@gmail.com>
2019-06-07 11:44:19 +0530

    Formatted files

--- solrj/src/main/java/com/sks/solr/SolrJClient.java
+++ solrj/src/main/java/com/sks/solr/SolrJClient.java
@@ -8,33 +8,33 @@ import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 
 public class SolrJClient {
-	
-	private final SolrClient _solrClient;
-	
-	private SolrJClient() {
-		List<String> zkHosts=new ArrayList<String>();  
-		zkHosts.add("zmc-zookeeper:2181");
-		Optional<String> empty = Optional.empty();
-		_solrClient = new CloudSolrClient.Builder(zkHosts,empty).build();
-	};
-	
-	private static SolrJClient _instance;
-	
-	public static SolrJClient getInstance() {
-		if (_instance == null) {
-			synchronized (SolrJClient.class) {
-				if (_instance == null) {
-					System.out.println("SolrJClient- going to create new instance.");
-					_instance = new SolrJClient();
-				}				
-			}
-		}
-		System.out.println("SolrJClient - returning instance");
-		return _instance;
-	}
-	
-	public SolrClient getClient()
-	{
-		return _solrClient;
-	}
+    
+    private final SolrClient _solrClient;
+    
+    private SolrJClient() {
+        List<String> zkHosts=new ArrayList<String>();  
+        zkHosts.add("zmc-zookeeper:2181");
+        Optional<String> empty = Optional.empty();
+        _solrClient = new CloudSolrClient.Builder(zkHosts,empty).build();
+    };
+
+    private static SolrJClient _instance;
+    
+    public static SolrJClient getInstance() {
+        if (_instance == null) {
+            synchronized (SolrJClient.class) {
+                if (_instance == null) {
+                    System.out.println("SolrJClient- going to create new instance.");
+                    _instance = new SolrJClient();
+                }
+            }
+        }
+        System.out.println("SolrJClient - returning instance");
+        return _instance;
+    }
+
+    public SolrClient getClient()
+    {
+        return _solrClient;
+    }
 }
